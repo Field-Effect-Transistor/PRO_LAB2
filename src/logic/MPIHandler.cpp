a0f03bb07d6811538106ccf36c6693bbdf93e114
Field-Effect-Transistor <vshapoval270@gmail.com>
2025-04-10 17:35:35 +0200

    boostTypesMPI

--- src/logic/MPIHandler.cpp
+++ src/logic/MPIHandler.cpp
@@ -37,3 +37,47 @@ int MPIHandler::getRank() {
 int MPIHandler::getSize() {
     return size;
 }
+
+void MPIHandler::sendVector(const vector<double>& vector, int n, int destRank) {
+    double* data = new double[n];
+    for (int i = 0; i < n; i++) {
+        data[i] = vector[i];
+    }
+    MPI_Send(data, n, MPI_DOUBLE, destRank, 0, MPI_COMM_WORLD);
+    delete[] data;
+}
+
+void MPIHandler::sendMatrix(const matrix<double>& matrix, int n, int destRank) {
+    double* data = new double[n * n];
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            data[i * n + j] = matrix(i, j);
+        }
+    }
+    MPI_Send(data, n * n, MPI_DOUBLE, destRank, 0, MPI_COMM_WORLD);
+    delete[] data;
+}
+
+vector<double> MPIHandler::receiveVector(int n, int sourceRank) {
+    double* data = new double[n];
+    MPI_Recv(data, n, MPI_DOUBLE, sourceRank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    vector<double> result(n);
+    for (int i = 0; i < n; i++) {
+        result[i] = data[i];
+    }
+    delete[] data;
+    return result;
+}
+
+matrix<double> MPIHandler::receiveMatrix(int n, int sourceRank) {
+    double* data = new double[n * n];
+    MPI_Recv(data, n * n, MPI_DOUBLE, sourceRank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    matrix<double> result(n, n);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            result(i, j) = data[i * n + j];
+        }
+    }
+    delete[] data;
+    return result;
+}
--- src/logic/MPIHandler.hpp
+++ src/logic/MPIHandler.hpp
@@ -2,11 +2,21 @@
 #pragma once
 #include <mpi.h>
 
+#include <boost/numeric/ublas/matrix.hpp>
+#include <boost/numeric/ublas/vector.hpp>
+
+using namespace boost::numeric::ublas;
+
 class MPIHandler {
 public:
     static MPIHandler& getInstance();
     static int getRank();
     static int getSize();
+
+    static void sendVector(const vector<double>& vector, int n, int destRank);
+    static void sendMatrix(const matrix<double>& matrix, int n, int destRank);
+    static vector<double> receiveVector(int n, int sourceRank);
+    static matrix<double> receiveMatrix(int n, int sourceRank);
 
 private:
     MPIHandler();
